840f59e5cd20974bb623785be697cb9962886507
Shru-03 <131282615+Shru-03@users.noreply.github.com>
2023-08-03 15:09:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./Client/build")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
@@ -35,8 +35,8 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 // rest api
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./Client/build/index.html"));
 });
 
 // Port
