6498d5aa042a6e949745e9118fc58c7e53445b96
Shru-03 <131282615+Shru-03@users.noreply.github.com>
2023-08-03 14:41:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoute.js";
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from "url";
 
 // config env
 dotenv.config();
 
 // database connection
 connectBD();
+
+// esModule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 // rest object
 const app = express();
